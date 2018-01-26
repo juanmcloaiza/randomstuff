f3487ab46da8462c2f823d81ae34755fe4114aa6
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-01-26 19:56:12 +0100

    JPG to CSV
    This program translates a JPG image to a CSV file. 
    When opened in Excel and assigned a color scale, you will see the image appear. 
    Works best with a black to white color scale.

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.inputBox = new System.Windows.Forms.TextBox();
+			this.outputBox = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.GObutton = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// inputBox
+			// 
+			this.inputBox.Location = new System.Drawing.Point(6, 23);
+			this.inputBox.Name = "inputBox";
+			this.inputBox.Size = new System.Drawing.Size(285, 20);
+			this.inputBox.TabIndex = 0;
+			this.inputBox.Text = "C:\\Users\\Juan\\Desktop\\ExcelChingon\\ElmoToilet.jpg";
+			// 
+			// outputBox
+			// 
+			this.outputBox.Location = new System.Drawing.Point(6, 76);
+			this.outputBox.Name = "outputBox";
+			this.outputBox.Size = new System.Drawing.Size(285, 20);
+			this.outputBox.TabIndex = 1;
+			this.outputBox.Text = "C:\\Users\\Juan\\Desktop\\ExcelChingon\\ElmoToilet.csv";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.GObutton);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.inputBox);
+			this.panel1.Controls.Add(this.outputBox);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(294, 162);
+			this.panel1.TabIndex = 2;
+			// 
+			// GObutton
+			// 
+			this.GObutton.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.GObutton.Location = new System.Drawing.Point(88, 107);
+			this.GObutton.Name = "GObutton";
+			this.GObutton.Size = new System.Drawing.Size(75, 42);
+			this.GObutton.TabIndex = 4;
+			this.GObutton.Text = "GO";
+			this.GObutton.UseVisualStyleBackColor = true;
+			this.GObutton.Click += new System.EventHandler(this.GObutton_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 60);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Output File";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(50, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Input File";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(318, 187);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox inputBox;
+		private System.Windows.Forms.TextBox outputBox;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button GObutton;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void GObutton_Click(object sender, EventArgs e)
+		{
+			Bitmap image = null;
+			double[,] IntArr = null;
+			try
+			{
+				image = loadImage(inputBox.Text);
+			}
+			catch
+			{
+				MessageBox.Show("Error loading image from input file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				IntArr = ImageToArray(image);
+			}
+			catch
+			{
+				MessageBox.Show("Error converting image to array", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				writeArrayToCsv(IntArr);
+			}
+			catch
+			{
+				MessageBox.Show("Error writing output file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			DialogResult result = MessageBox.Show("Go to Output Folder?", "Finished", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			if (result == DialogResult.Yes)
+				OpenExplorer(outputBox.Text);
+		}
+
+		private void OpenExplorer(string path)
+		{
+			if (File.Exists(path))
+			{
+				var argument = "/select, \"" + path + "\"";
+				Process.Start("explorer.exe", argument);
+			}
+			else
+			{
+				MessageBox.Show("File " + path + " not found.", "File not found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		private void writeArrayToCsv(double [,] arr)
+		{
+			using (StreamWriter sW = new StreamWriter(outputBox.Text))
+			{
+				List<string> line = new List<string>();
+				for (int i = 0; i < arr.GetLength(0); i++)
+				{
+					for (int j = 0; j < arr.GetLength(1); j++)
+					{
+						line.Add(arr[i, j].ToString());
+					}
+					sW.WriteLine(string.Join(";",line));
+					line.Clear();
+				}
+			}
+		}
+
+		private Bitmap loadImage(string path)
+		{
+			if (path == null || path.Equals("") || !File.Exists(path))
+			{
+				return null;
+			}
+			else
+			{
+				byte[] fileBytes = File.ReadAllBytes(path);
+				MemoryStream imageStream = new MemoryStream(fileBytes);
+				Image imageImage = Image.FromStream(imageStream);
+				int heigth = imageImage.Height;
+				int width = imageImage.Width;
+				Bitmap b = new Bitmap(imageImage);
+				return b;
+			}
+		}
+
+		private double[,] ImageToArray(Bitmap image)
+		{
+
+			Bitmap bitmap_data = image; //.LockBits(new Rectangle(0, 0, _foto_franjas_original.Width, _foto_franjas_original.Height),
+												//ImageLockMode.ReadOnly, _foto_franjas_original.PixelFormat);
+
+			int nbytes = bitmap_data.Height * bitmap_data.Width;
+			byte[] imageBytes = new byte[nbytes];
+
+			using (MemoryStream imageStream = new MemoryStream())
+			{
+				// Save bitmap in some format.
+				image.Save(imageStream, System.Drawing.Imaging.ImageFormat.Bmp);
+				imageStream.Position = 0;
+
+				// Do something with the memory stream. For example:
+				imageBytes = imageStream.ToArray();
+				// Save bytes to the database.
+			}
+
+
+			double red;
+			double green;
+			double blue;
+			double gray;
+
+			var grayscale_array = new Double[bitmap_data.Height, bitmap_data.Width];
+			int pixelsize = Image.GetPixelFormatSize(bitmap_data.PixelFormat) / 8;
+
+
+
+			for (int I = 0; I < bitmap_data.Height; I++)
+				{
+					for (int J = 0; J < bitmap_data.Width; J++)
+					{
+						int position = pixelsize * 13 + (I * bitmap_data.Width * pixelsize) + (J * pixelsize);
+						blue = imageBytes[position+1];
+					    green = imageBytes[position + 2];
+					    red = imageBytes[position + 3];
+						gray = 0.299 * red + 0.587 * green + 0.114 * blue;
+						int Ireversed = bitmap_data.Height - I - 1;
+						grayscale_array[Ireversed, J] = gray;
+					}
+				}
+
+			return grayscale_array;
+		}
+
+	}
+}
