fb0e2799db3d901dd3b30d34398746ae5cd59958
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-07-17 19:00:51 +0200

    fixed bug in pack.sh: path source/build --> /build

--- PlayWithDocker/pack.sh
+++ PlayWithDocker/pack.sh
@@ -2,4 +2,4 @@ distro=$1
 echo "packing ${distro} ..."
 docker exec ${distro}-container bash -c "cd build; cpack"
 docker exec ${distro}-container bash -c "cd build; mv *.deb ./BornAgain-dev-${distro}.deb"
-docker cp   ${distro}-container:/source/build/BornAgain-dev-${distro}.deb ./
+docker cp   ${distro}-container:/build/BornAgain-dev-${distro}.deb ./
